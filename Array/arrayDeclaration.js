24757904208cd284f6a2c8f6e4781e6c63bc7595
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-05-23 11:42:03 +0530

    Array declaration using literal and constructor

---
+++ Array/arrayDeclaration.js
@@ -0,0 +1,9 @@
+// using array literal
+const arr1 = [1, 2, 3, 4, 5, 6]
+console.log(arr1)
+
+// using array constructor
+const arr2 = new Array(1, 2, 3, 4, 5, 6)
+console.log(arr2)
+
+console.log(Array.isArray(arr1))
