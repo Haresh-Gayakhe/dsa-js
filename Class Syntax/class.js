e8823e66e378abefb7dc92e7386b0cad278e1bc1
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-06-01 21:50:14 +0530

    instance method in class

--- Class Syntax/class.js
+++ Class Syntax/class.js
@@ -3,6 +3,30 @@ class Student{
     constructor(firstName, lastName){
         this.firstName = firstName
         this.lastName = lastName
+        this.lateComing = 0
+        this.score = []
+    }
+
+    // Create instance method
+    fullName(){
+        return `Your full name is ${this.firstName} ${this.lastName}`
+    }
+
+    lateComingCount(){
+        if(this.lateComing == 3){
+            return `You are not allowed to enter class until giving valid reason`
+        }
+        this.lateComing += 1
+        return `You are late for ${this.lateComing} times`
+    }
+
+    addScore(score){
+        this.score.push(score)
+    }
+
+    getAverageScore(){
+        let total = this.score.reduce((a, b) => a + b)
+        return (total/this.score.length).toFixed(2)
     }
 }
 
@@ -11,9 +35,24 @@ let haresh = new Student("Haresh", "Gayakhe")
 let john = new Student("John", "Doe", 28)
 
 // Access instance
-console.log(haresh)
-console.log(john)
+// console.log(haresh)
+// console.log(john)
 
 // Add other fields
-haresh.age = 27
+// haresh.age = 27
+// console.log(haresh)
+
+// Access instance method
+console.log(haresh.fullName())
+
+console.log(haresh.lateComingCount())
+console.log(haresh.lateComingCount())
+console.log(haresh.lateComingCount())
+console.log(haresh.lateComingCount())
+
+haresh.addScore(87)
+haresh.addScore(98)
+
+console.log(haresh.getAverageScore())
+
 console.log(haresh)
