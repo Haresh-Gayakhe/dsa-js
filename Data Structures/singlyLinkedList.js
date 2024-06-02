791aebb94233b8f41f365f4f3646c99344ae7707
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-06-02 22:58:23 +0530

    class node, singlyLinkedList created

---
+++ Data Structures/singlyLinkedList.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val){
+    
+  }
+}
