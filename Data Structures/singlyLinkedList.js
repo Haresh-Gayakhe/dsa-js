84ae5a19973cac58a6f9d299c5d80a501a2c8cd6
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-06-02 23:26:43 +0530

    push logic implemented

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -11,7 +11,18 @@ class SinglyLinkList {
     this.tail = null;
     this.length = 0;
   }
+
   push(val){
-    
+    let newNode = new Node(val)
+    if(!this.head){
+        this.head = newNode
+        this.tail = newNode
+    }
+    else{
+        this.tail.next = newNode
+        this.tail = newNode
+    }
+    this.length++
+    return this
   }
 }
