d68b7d51e0810b69f818deba58ff45df9c3c98ea
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-06-03 00:15:36 +0530

    pop logic implemented

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -12,17 +12,40 @@ class SinglyLinkList {
     this.length = 0;
   }
 
-  push(val){
-    let newNode = new Node(val)
-    if(!this.head){
-        this.head = newNode
-        this.tail = newNode
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    else{
-        this.tail.next = newNode
-        this.tail = newNode
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
-    this.length++
-    return this
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length == 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
   }
 }
+
+
+let list = new SinglyLinkList()
+list.push("Hello")
+list.push("World")
+list.push("!")
